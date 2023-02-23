515e07a428d688aa681baa9095f1009de8b7e0a1
mochi22 <jdhdbemd@gmail.com>
2023-02-23 19:45:26 +0900

    'kakunin'

--- classification/main.py
+++ classification/main.py
@@ -343,6 +343,8 @@ def main(args):
 
         # 全結合層の重み行列のサイズを修正する
         old_weights = model_without_ddp.proj_head[0].weight.data  # 旧重みを保存する
+        print(old_weights)
+        print(old_weights.size())
         new_weights = torch.nn.Parameter(torch.zeros(num_classes, 1024))
         new_weights.data[:old_weights.shape[0], :] = old_weights
         model_without_ddp.proj_head[0].weight = new_weights  # 新しい重みをセットする
